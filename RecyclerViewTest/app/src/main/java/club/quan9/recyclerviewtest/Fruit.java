9d5b1ac166add841c226b40ed2ad4ed8dda607fe
ws752499660 <ws752499660@foxmail.com>
2018-03-20 16:45:50 +0800

    emm

---
+++ RecyclerViewTest/app/src/main/java/club/quan9/recyclerviewtest/Fruit.java
@@ -0,0 +1,17 @@
+package club.quan9.recyclerviewtest;
+
+/**
+ * Created by wily on 2018/3/19.
+ */
+
+public class Fruit
+{
+    private String Name;
+    private int ImageId;
+    public Fruit(String Name,int ImageId)
+    {
+        this.Name=Name;
+        this.ImageId=ImageId;
+    }
+
+}
